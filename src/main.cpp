43a1f063c5989a6f9405e1054de64df7e7ab53f3
Paul <paul.turkovskiy@gmail.com>
2025-08-01 00:51:33 +0300

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
-#include "command_dispatcher.h"
-
 // https://github.com/brainboxdotcc/DPP/pull/1468
 #include <dpp/dpp.h>
 #ifdef _WIN32
 #undef poll
 #undef pollfd
 #endif
-
 #include <crow.h>
 
-int main() {
-    dpp::cluster bot(std::getenv("TOKEN"));
+#include <utility/utils.h>
+#include <config/config_env.h>
+#include <log/logger_adapter.h>
 
-    bot.on_log(dpp::utility::cout_logger());
+#include "command_dispatcher.h"
+
+int main() {
+    g_Logger.Log(LogLevel::Trace, "Logger test");
+
+    std::unique_ptr<IConfig> Config = std::make_unique<EnvConfig>();
+
+    std::string token = Config->GetString("DC_TOKEN", "");
+    dpp::snowflake channel = Config->GetString("DC_CHANNEL", "");
+    int port = Config->GetInt("DC_WEB_PORT", 18080);
+
+    if(token.empty() || channel.empty())
+    {
+        g_Logger.Log(LogLevel::Critical, "One of essential config vars are missing");
+        return 1;
+    }
+
+    dpp::cluster bot(token);
+
+    bot.on_log(DppLogAdapter::Logger());
     bot.on_slashcommand(CCommandDispatcher::DispatchCommand);
 
     bot.on_ready([&bot](const dpp::ready_t& event) {
@@ -26,14 +43,16 @@ int main() {
     });
 
     crow::SimpleApp app;
+    CrowLogAdapter CrowLogger;
+    crow::logger::setHandler(&CrowLogger);
 
     CROW_ROUTE(app, "/send/<string>")
-            ([&bot](const crow::request& req, crow::response& res, const std::string& msg) {
-                bot.message_create(dpp::message(dpp::snowflake(std::getenv("CHANNEL")), msg));
+            ([&bot, channel](const crow::request& req, crow::response& res, const std::string& msg) {
+                bot.message_create(dpp::message(channel, msg));
                 res.end("Message sent to Discord!");
             });
 
-    std::cout << "HTTP server running on http://localhost:18080\n";
+    g_Logger.Log(LogLevel::Info, "HTTP server running on http://localhost:{}", port);
     app.port(18080).multithreaded().run();
 
     discord_thread.join();
